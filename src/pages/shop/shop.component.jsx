9a2fad2b6cdeb135a73a6b7ac682f30ce4b10a3a
Anthony Colbert <tony41490@yahoo.com>
2020-05-17 14:35:24 -0400

    added collections-overview component for displaying shop collections

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import {connect} from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import {selectCollections} from '../../redux/shop/shop.selectors';
 
 const ShopPage = ({collections}) => (
             <div className='shop-page'>
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps}/>
-                    ))
-                }
+                <CollectionsOverview />
                 
             </div>
 );
