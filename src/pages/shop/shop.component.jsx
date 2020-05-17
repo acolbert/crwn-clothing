eb189bf4ca94fece66a5120281947c5acfb2291b
Anthony Colbert <tony41490@yahoo.com>
2020-05-17 18:02:03 -0400

    moved shoping data into reducer and updated array to map, then modified pages for the change

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import {connect} from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import {selectCollections} from '../../redux/shop/shop.selectors';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({collections}) => (
+import {Route} from 'react-router-dom';
+
+const ShopPage = ({match}) => (
             <div className='shop-page'>
-                <CollectionsOverview />
-                
+                <Route exact path={`${match.path}`} component={CollectionsOverview} />
+                <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
             </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-    collections : selectCollections
-});
 
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
